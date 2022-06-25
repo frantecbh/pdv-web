92bda433643794cf34fde1e33ba5e0f8012d91dd
frantecbh <franciscomenezesbh@gmail.com>
2022-06-25 11:49:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header"
+import { GlobalStyle } from "./styles/global"
+
+
+function App() {
+
+  return (
+    <>
+        <Header />
+        <GlobalStyle />
+    </>
+  
+  )
+
+}
+
+export default App
